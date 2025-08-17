de034bcf461ac17f09d967ad024fc8896f535b91
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-08-17 19:41:02 +0000

    2. Set up Header, Footer, Layout and landing page

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC = () => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
-      Button
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+    >
+      {title}
     </button>
   );
 };
